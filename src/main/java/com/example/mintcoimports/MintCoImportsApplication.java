5f7ae01859d7887c4dd8eca5ace18652be030582
Rodrigo Marquez <rodrigo.nmn.marquez@gmail.com>
2023-03-31 15:03:22 -0500

    autosave script

---
+++ src/main/java/com/example/mintcoimports/MintCoImportsApplication.java
@@ -0,0 +1,13 @@
+package com.example.mintcoimports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MintCoImportsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MintCoImportsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/mintcoimports/MintCoImportsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mintcoimports;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MintCoImportsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
